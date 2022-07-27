fbdd08a97e50c96db3c0d6670925f78b9108a3ef
Lucas Lima <id.lucas@outlook.com.br>
2022-07-27 12:37:16 -0300

    chore: configure eslint

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,10 @@
+import styled from "styled-components";
+
 const Dashboard = () => {
+  for (let i = 0; i < 12; i++) {
+    console.log(i);
+  }
+
   return (
     <div>
       <h1>Bem vindo ao Dashboard</h1>
