b589834ed2ff47bd515f1adf5fefa2065e37d379
Lucas Lima <id.lucas@outlook.com.br>
2022-07-27 12:54:13 -0300

    chore: configure lint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 const Dashboard = () => {
-  for (let i = 0; i < 12; i++) {
-    console.log(i);
-  }
-
   return (
     <div>
       <h1>Bem vindo ao Dashboard</h1>
