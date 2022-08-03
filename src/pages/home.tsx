010ef16800ea2e5275640ca13e6ce42efa31c08e
judsonkerller <judsonkerller@gmail.com>
2022-08-03 14:36:15 -0300

    fix: remove the component RM from home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable prettier/prettier */
-import Footer from "@components/Footer";
 import Header from "@components/Header";
-import RelevantMatters from "@components/RelevantMatters";
 import * as S from "@styles/Pages/home";
 
 const Home = () => {
   return (
     <S.Wrapper>
       <Header />
-      <Footer />
     </S.Wrapper>
   );
 };
