005344eb24bae5437abc6b492148c095e961d4ab
ViGF <vitoriogdef@gmail.com>
2023-05-29 14:01:55 -0300

    feat about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,48 @@
+import * as S from "@styles/Pages/about";
+import Image from "next/image";
+
+import peopleImg from "../../public/assets/about-people.png";
+
+const About = () => {
+  return (
+    <>
+      <S.Main>
+        <div>
+          <h1>
+            Nosso foco é transformar
+            <br /> vidas!
+          </h1>
+          <p>
+            Ajudamos você Desenvolvedor Jr a conseguir sua primeira vaga no
+            mercado de trabalho, com dicas e anúncios de vagas para você
+            alavangar a sua carreira.
+          </p>
+        </div>
+      </S.Main>
+      <S.Container>
+        <div>
+          <h2>Quem somos?</h2>
+          <p>
+            Somos uma equipe de desenvolvedores que se uniram com o propósito de
+            alavancar a carreira daqueles que estão iniciando na área de
+            programação, e se sentem meio perdidos sobre por onde começar a
+            estudar e onde encontrar vagas para o primeiro emprego. Sabemos o
+            quanto o começo dessa caminhada é difícil, e nos unimos com o
+            objetivo de facilitar a sua jornada como desenvolvedor. Vamos
+            avançar juntos?
+          </p>
+        </div>
+        <div>
+          <Image
+            src={peopleImg}
+            alt="Foto de homem em um escritoório sorrindo para a câmera e segurando um notebook"
+            width={436}
+            height={476}
+          />
+        </div>
+      </S.Container>
+    </>
+  );
+};
+
+export default About;
---
+++ src/styles/Pages/about.ts
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+import bgImage from "../../../public/assets/about-bg1.png";
+
+export const Main = styled.main`
+  background-image: url(${bgImage.src});
+  color: var(--white);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: right top;
+  display: flex;
+  align-items: end;
+  flex: 1;
+  margin-top: 5rem;
+  max-height: 90vh;
+  height: calc(100vh - 4rem);
+
+  div {
+    max-width: 32.75rem;
+    padding-left: 9.75rem;
+    padding-bottom: 7rem;
+
+    h1 {
+      font-size: 3.1rem;
+    }
+
+    p {
+      margin-top: 0.75rem;
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (max-width: 720px) {
+    justify-content: center;
+    align-items: center;
+
+    div {
+      padding: unset;
+
+      h1,
+      p {
+        text-shadow: 0 0 1.25rem black;
+      }
+    }
+  }
+`;
+
+export const Container = styled.div`
+  background-color: var(--light-900);
+  height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-column-gap: 2rem;
+  padding-inline: 9.6rem;
+  align-items: center;
+  justify-items: center;
+
+  div {
+    h2 {
+      font-size: 1.9rem;
+    }
+
+    p {
+      margin-top: 1.75rem;
+      font-size: 1.2rem;
+    }
+
+    img {
+      width: 27.25rem;
+      height: 29.75rem;
+      filter: drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.25));
+      border-radius: 24px;
+    }
+  }
+
+  @media (max-width: 720px) {
+    grid-template-columns: unset;
+    padding-inline: 3rem;
+
+    div {
+      h2 {
+        font-size: 2rem;
+      }
+
+      p {
+        font-size: 1.3rem;
+      }
+    }
+
+    div:nth-child(2) {
+      display: none;
+    }
+  }
+`;
